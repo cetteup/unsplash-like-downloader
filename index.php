7ead3d2ac870ff1d5cd69435009a1c0143ea0494
taduuda <taduuda@user210314.de>
2016-02-10 21:33:29 +0100

    Changed script to only download a user's liked images

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
+// Set username
+$username = 'taduuda';
 
 // Extend maximum execution time
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 500);
 
 // Load PHP Simple HTML DOM Parser Class
 include_once('lib/simple_html_dom.php');
 
 // Initialize PHP Simple HTML DOM Parser
 $unsplash = new simple_html_dom();
-$unsplash->load_file('https://unsplash.com/');
+$unsplash->load_file('https://unsplash.com/' . $username . '/likes/');
 
 // Check number of pages
 $page = $unsplash->find('.pagination a[!rel]', -1);
@@ -17,55 +19,36 @@ $page = $unsplash->find('.pagination a[!rel]', -1);
 // Loop all pages and save photos
 for($i=1; $i <= $page->plaintext; $i++) {
 
-	savePhotos($i);
+	savePhotos($i,$username);
 	
 	echo 'Page n' . $i . ' OK</br>';
 	
 }
 
-// Archive all the files inside img folder into a ZIP file
-zipFiles();
-
-
-function savePhotos($pageNbr) {
+function savePhotos($pageNbr,$username) {
 
 	// If img folder doesn't exist we create it
 	if (!file_exists('img'))
 		mkdir('img');
 	
 	$photos = new simple_html_dom();
-	$photos->load_file('https://unsplash.com/?page='.$pageNbr);
+	$photos->load_file('https://unsplash.com/' . $username . '/likes/?page='.$pageNbr);
 	
-	foreach($photos->find('.photo-container img') as $photo) {
+	foreach($photos->find('.photo__image-container img') as $photo) {
 	
 		// Remove attributes in URL
 		$img = explode('?', $photo->src);
+        
+        // Set name of current img as filename
+        $filename = 'img/' . basename($img[0]) .'.jpg';
 
-		// Save images into img folder
-		file_put_contents('img/' . basename($img[0]) .'.jpg', file_get_contents($img[0]));
+		// If image does not exist we download it
+        if(!file_exists($filename)) {
+            // Save images into img folder
+		    file_put_contents($filename, file_get_contents($img[0]));
+        }
 	}
 	
-	
-}
-
-function zipFiles() {
-
-	$zip = new ZipArchive();
-	
-	if($zip->open('unsplash.zip', ZipArchive::CREATE) === true)
-	{
-		
-		// List all the files inside img folder
-		$files = scandir('img');
-		
-		foreach($files as $file) {
-		
-			// Add file to the archive
-			$zip->addFile('img/' . $file);
-		}
-		
-	$zip->close();
-	}
 	
 }
 
